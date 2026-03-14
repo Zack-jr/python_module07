a647f7a8a937fd388adcf3f4ab68301529d4895f
Zack <girardzack04@example.com>
2026-03-14 01:25:17 +0100

    missing check and flake8

--- ex4/TournamentCard.py
+++ ex4/TournamentCard.py
@@ -2,17 +2,36 @@ from . import Card, Combatable, Rankable
 
 class TournamentCard(Card, Combatable, Rankable):
 
-    def __init__(name: str, cost: int, rarity: str):
+    def __init__(self, name: str, cost: int, rarity: str, id: str, rating: int):
         super().__init__(name, cost, rarity)
-
+        self.id = id
+        self.wins = 0
+        self.losses = 0
+        self.rating = rating
+    
     def play(self, game_state: dict) -> dict:
-        pass
+        if "active" in game_state.values():
+            return {f"Playing {self.name}."}
 
     def attack(self, target) -> dict:
-        pass
+        return {"attacker": self.name, "defender": target}
 
     def defend(self, incoming_damage: int) -> dict:
-        pass
+        return {"defender": self.name, "damage_taken": incoming_damage}
 
     def get_combat_stats(self) -> dict:
-        pass
+        return {"wins": self.wins, "losses": self.losses}
+    
+    def calculate_rating(self) -> int:
+        return self.rating
+    
+    def update_wins(self, wins: int) -> None:
+        self.wins =+ wins
+        self.rating += 16 * wins
+    
+    def update_losses(self, losses: int) -> None:
+        self.losses += losses
+        self.rating -= 16 * losses
+    
+    def get_rank_info(self) -> dict:
+        return {"Rating": self.rating}
--- ex4/__init__.py
+++ ex4/__init__.py
@@ -1,3 +1,5 @@
 from ex0.Card import Card
 from ex2.Combatable import Combatable
 from ex4.Rankable import Rankable
+from ex4.TournamentCard import TournamentCard
+from ex4.TournamentPlatform import TournamentPlatform
